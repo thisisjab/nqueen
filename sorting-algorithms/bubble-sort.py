4c56c4ff9710e749c671251abeefda2f6ec4a122
thisisjab <heythisisjab@gmail.com>
2023-11-08 18:31:23 +0330

    clean up bubble sort file

--- sorting-algorithms/bubble-sort.py
+++ sorting-algorithms/bubble-sort.py
@@ -1,9 +1,6 @@
 """Bubble Sort"""
 
 
-from termcolor import colored
-
-
 def bubble_sort(value: list):
     """Sort a list using bubble sort algorithm.
 
@@ -14,12 +11,9 @@ def bubble_sort(value: list):
 
     for i in range(n):
         for j in range(0, n - i - 1):
-            print(colored(f'Current array: {value}', 'yellow'))
-            print(f'Comparing value[{j}]={value[j]} with value[{j+1}]={value[j+1]}', end='\n')
             if value[j] > value[j + 1]:
-                print(colored(f' -- Changing value[{j}]={value[j]} with value[{j+1}]={value[j+1]}', 'green'))
                 value[j], value[j + 1] = value[j + 1], value[j]
-            print()
+
 
 test_value = [5, 54, 3, 6, 2, 64]
 
